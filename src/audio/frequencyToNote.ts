63a901dbc294b1aafad7712ba4bcef61d8c9b1d5
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-22 20:06:54 -0300

    Separate note name from note octave.

--- src/audio/frequencyToNote.ts
+++ src/audio/frequencyToNote.ts
@@ -2,28 +2,30 @@ import { A4_FREQUENCY, A4_MIDI, ALLOWED_DEVIATION, NOTES } from "./audioConstant
 
 export interface NoteInfo {
   note: string
+  octave: number
   frequency: number
   cents: number
   isInTune: boolean
 }
 
+// TODO: Check this whole logic...
 export function frequencyToNote(frequency: number): NoteInfo {
   console.log(`Freq: ${frequency}`)
   // Find MIDI note number:
   const midiNote =  A4_MIDI + (12 * Math.log2(frequency / A4_FREQUENCY))
-  const closestMidiNote = Math.round(midiNote)
+  const closestMidiNoteNumber = Math.round(midiNote)
 
   // Find octave.
-  const octave = Math.floor((closestMidiNote - 12) / 12)
-  const noteIndex = ((closestMidiNote - 12) % 12 + 12) % 12
-  const noteName = NOTES[noteIndex]
+  const octave = Math.floor(closestMidiNoteNumber / 12)
+  const noteName = NOTES[closestMidiNoteNumber % 12]
 
   // How many cents sharp/flat is the note?
-  const expectedFrequency = A4_FREQUENCY * Math.pow(2, (closestMidiNote - 69) / 12)
+  const expectedFrequency = A4_FREQUENCY * Math.pow(2, (closestMidiNoteNumber - A4_MIDI) / 12)
   const cents = Math.round(1200 * Math.log2(frequency / expectedFrequency))
 
   return {
-    note: `${noteName}${octave}`,
+    note: noteName,
+    octave,
     frequency,
     cents,
     isInTune: Math.abs(cents) <= ALLOWED_DEVIATION,
