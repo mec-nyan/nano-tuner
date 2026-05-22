4b347154e75e0772bc283a9579276511ec9a9aee
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-22 20:57:26 -0300

    Work on the Display component.

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,15 +1,29 @@
 import type { NoteInfo } from "../audio/frequencyToNote";
 
+import './Display.css'
+
 export default function Display({ noteInfo }: { noteInfo: NoteInfo | null}) {
   return (
     <div className='outer-frame'>
       <div className='inner-frame'>
         <div className='display'>
+          <div className='top-info-bar'>
+            <div className='info-flat'>♭</div>
+            <div className='info-fork'>A4 = 440Hz</div>
+            <div className='info-sharp'>♯</div>
+          </div>
           <div className='note'>
+            {/*
             <span className='note-name'>{noteInfo ? noteInfo.note : '-'}</span>
             <span className='note-index'>{noteInfo ? noteInfo.octave : '-'}</span>
+            */}
+            <span className='note-name'>{noteInfo ? noteInfo.note : 'A'}</span>
+            <span className='note-index'>{noteInfo ? noteInfo.octave : '2'}</span>
           </div>
+          {/*
           <div className='note-frequency'>{noteInfo ? noteInfo.frequency.toFixed(2) : '-'} Hz</div>
+          */}
+          <div className='note-frequency'>{noteInfo ? noteInfo.frequency.toFixed(2) : '110.00'} Hz</div>
         </div>
       </div>
     </div>
