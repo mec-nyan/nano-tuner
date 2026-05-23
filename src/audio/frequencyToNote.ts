cc801388985641475938fad67086bc7fb0828083
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-23 20:00:39 -0300

    Small fixes in note calculation.

--- src/audio/frequencyToNote.ts
+++ src/audio/frequencyToNote.ts
@@ -14,20 +14,24 @@ export interface NoteInfo {
 // TODO: Check this whole logic...
 export function frequencyToNote(frequency: number | null): NoteInfo | null {
   // console.log(`Freq: ${frequency}`)
-  if (frequency === null) {
+  if (
+    frequency === null ||
+    !Number.isFinite(frequency) ||
+    frequency <= 0
+  ) {
     return null
   }
   // Find MIDI note number:
-  const midiNote =  A4_MIDI + (12 * Math.log2(frequency / A4_FREQUENCY))
-  const closestMidiNoteNumber = Math.round(midiNote)
+  const closestMidiNoteNumber = getClosestMIDINote(frequency)
 
   // Find octave.
-  const octave = Math.floor(closestMidiNoteNumber / 12)
+  const octave = Math.floor(closestMidiNoteNumber / 12) - 1
   const noteName = NOTES[closestMidiNoteNumber % 12]
 
   // How many cents sharp/flat is the note?
-  const expectedFrequency = A4_FREQUENCY * Math.pow(2, (closestMidiNoteNumber - A4_MIDI) / 12)
-  const cents = Math.round(1200 * Math.log2(frequency / expectedFrequency))
+  const targetNoteFrequency = A4_FREQUENCY * Math.pow(2, (closestMidiNoteNumber - A4_MIDI) / 12)
+  // TODO: Use 'cents' with decimal places.
+  const cents = Math.round(1200 * Math.log2(frequency / targetNoteFrequency))
 
   return {
     note: noteName.name,
@@ -37,4 +41,10 @@ export function frequencyToNote(frequency: number | null): NoteInfo | null {
     cents,
     isInTune: Math.abs(cents) <= ALLOWED_DEVIATION,
   }
+}
+
+function getClosestMIDINote(frequency: number): number {
+  const MIDIWithDecimals =  A4_MIDI + (12 * Math.log2(frequency / A4_FREQUENCY))
+  const closestMidiNoteNumber = Math.round(MIDIWithDecimals)
+  return closestMidiNoteNumber
 }
