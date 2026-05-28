e5d53cf1e7f4c3d7ad7e73fb163e3079df219a24
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-28 18:49:07 -0300

    Funcionality: hide error message.

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './ErrorMessage.css'
 
 interface ErrorMessageProps {
@@ -7,31 +8,39 @@ interface ErrorMessageProps {
 const microphoneAccessError = /^Couldn't access microphone/
 
 export default function ErrorMessage({ error }: ErrorMessageProps) {
+  const [hidden, setHidden] = useState(false)
+
   return (
-    <div className='error-container'>
-      <div className='error-message-border'>
-        <div className='error-message'>
-          <h2>Oh f**k!</h2>
-          {error !== null && microphoneAccessError.test(error) ? (
-            <p>
-              Sorry mate, it seems we couldn't access the microphone on your
-              device...
-              <br />
-              <br />
-              Try reloading and allowing the app to use the microphone.
-            </p>
-          ) : (
-            <p>
-              Sorry mate, it seems that something didn't quite work as
-              expected...
-              <br />
-              <br />
-              Try reloading and allowing the app to use the microphone.
-            </p>
-          )}
-          <div className='error-close'>Got it!</div>
+    <>
+      {hidden === false && (
+        <div className='error-container'>
+          <div className='error-message-border'>
+            <div className='error-message'>
+              <h2>Oh f**k!</h2>
+              {error !== null && microphoneAccessError.test(error) ? (
+                <p>
+                  Sorry mate, it seems we couldn't access the microphone on your
+                  device...
+                  <br />
+                  <br />
+                  Try reloading and allowing the app to use the microphone.
+                </p>
+              ) : (
+                <p>
+                  Sorry mate, it seems that something didn't quite work as
+                  expected...
+                  <br />
+                  <br />
+                  Try reloading and allowing the app to use the microphone.
+                </p>
+              )}
+              <div className='error-close' onClick={() => setHidden(true)}>
+                Got it!
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
