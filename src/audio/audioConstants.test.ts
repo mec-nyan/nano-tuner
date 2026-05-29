43a82e174610e364ee7785473cc685d73d922ca5
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-29 20:43:27 -0300

    Correct tests names.

--- src/audio/audioConstants.test.ts
+++ src/audio/audioConstants.test.ts
@@ -2,17 +2,19 @@ import { describe, expect, it } from 'vitest'
 import { accidentalToString } from './audioConstants'
 
 describe('Gets the appropriate musical symbol', () => {
-  it('Music flat symbol', () => {
+  it('should return the musical flat symbol', () => {
     const flatSymbol = accidentalToString('flat')
 
     expect(flatSymbol).toBe('♭')
   })
-  it('Music sharp symbol', () => {
+
+  it('should return the musical sharp symbol', () => {
     const sharpSymbol = accidentalToString('sharp')
 
     expect(sharpSymbol).toBe('♯')
   })
-  it('None', () => {
+
+  it('should return an empty string', () => {
     const noneSymbol = accidentalToString(null)
 
     expect(noneSymbol).toBe('')
